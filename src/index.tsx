3eaf3de0db1b6908af18ae0c1dd9208060e89ab7
nadavl <nadav@nsoft.co.il>
2016-04-07 17:52:22 +0300

    broken into components

--- src/index.tsx
+++ src/index.tsx
@@ -28,36 +28,53 @@ class TimerView extends React.Component<{appState: AppState}, {}> {
     this.props.appState.resetTimer();
   }
 }
-;
 
-var PanZoom = React.createClass({
-  render: function () {
+class Toolbar extends React.Component<{}, {}> {
+  render () {
     return (
-
       <div className="cy-panzoom" style={{position: 'absolute'}}>
+        <Pan/>
+        <Slider/>
+      </div>
+    );
+  }
+}
+
+class Slider extends React.Component<{}, {}> {
+  render () {
+    return (
+      <div>
+        <div className="cy-panzoom-reset cy-panzoom-zoom-button"><span className="icon fa fa-expand"/></div>
         <div className="cy-panzoom-zoom-in cy-panzoom-zoom-button"><span className="icon fa fa-plus"/></div>
         <div className="cy-panzoom-zoom-out cy-panzoom-zoom-button"><span className="icon fa fa-minus"/></div>
-        <div className="cy-panzoom-reset cy-panzoom-zoom-button"><span className="icon fa fa-expand"/></div>
         <div className="cy-panzoom-slider">
           <div className="cy-panzoom-slider-background"/>
           <div className="cy-panzoom-slider-handle" style={{top: '32.2203px'}}><span className="icon fa fa-minus"/>
           </div>
           <div className="cy-panzoom-no-zoom-tick" style={{top: 43}}/>
         </div>
-        <div className="cy-panzoom-panner">
-          <div className="cy-panzoom-panner-handle"/>
-          <div className="cy-panzoom-pan-up cy-panzoom-pan-button"/>
-          <div className="cy-panzoom-pan-down cy-panzoom-pan-button"/>
-          <div className="cy-panzoom-pan-left cy-panzoom-pan-button"/>
-          <div className="cy-panzoom-pan-right cy-panzoom-pan-button"/>
-          <div className="cy-panzoom-pan-indicator" style={{display: 'none'}}/>
-        </div>
       </div>
-    );
+    )
   }
-});
+}
 
-ReactDOM.render(<PanZoom />, document.getElementById('root'));
+class Pan extends React.Component<{}, {}> {
+  render () {
+    return (
+      <div className="cy-panzoom-panner">
+        <div className="cy-panzoom-panner-handle"/>
+        <div className="cy-panzoom-pan-up cy-panzoom-pan-button"/>
+        <div className="cy-panzoom-pan-down cy-panzoom-pan-button"/>
+        <div className="cy-panzoom-pan-left cy-panzoom-pan-button"/>
+        <div className="cy-panzoom-pan-right cy-panzoom-pan-button"/>
+        <div className="cy-panzoom-pan-indicator" style={{display: 'none'}}/>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<Toolbar />, document.getElementById('root'));
 
 // const appState = new AppState();
 // ReactDOM.render(<TimerView appState={appState}/>, document.getElementById('root'));
